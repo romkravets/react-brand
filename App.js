2d3682db0bfbb2d45a0823677e86fdee077507b1
romkravets <romann.kravets@gmail.com>
2020-07-21 15:12:44 +0300

    Add Modal component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
 
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
 
 
@@ -15,6 +16,7 @@ export default function App() {
       [...currentGoals,
       { id: Math.random().toString(), value: goalTitle}
       ]);
+    setIsAddMode(false);
   };
 
   const removeGoalHandler = goalId => {
@@ -24,9 +26,14 @@ export default function App() {
       })
   }
 
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  }
+
   return (
       <View style={styles.screen}>
-          <GoalInput onAddGoal={addGoalHandler}/>
+        <Button title="Add New Goal" onPress={() => setIsAddMode(true)}/>
+          <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionHandler}/>
           <FlatList
               keyExtractor={(item, index) => item.id}
               data={courseGoals}
