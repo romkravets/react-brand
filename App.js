7b0de3f58a4d3c5b14136e370c3efabd2f100299
romkravets <romann.kravets@gmail.com>
2020-07-25 20:16:16 +0300

    card

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
 import React, {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Hello")
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <StatusBar style="auto" />
-      <Button title="Change Text" onPress={() => setOutputText('World')}/>
+    <View style={styles.screen}>
+        <Header title="Guess a Number"/>
+        <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+    screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
