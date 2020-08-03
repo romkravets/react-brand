da43320bdfc84ef657a46f0b7a54dbb86cff7d53
romkravets <romann.kravets@gmail.com>
2020-08-03 12:30:43 +0300

    finish app

--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,44 @@
-import React from "react";
-import {View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 
+import TitleText from './TitleText';
 import Colors from '../constants/colors';
 
-const Header = (props) => {
-    return (
-        <View style={styles.header}>
-            <Text style={styles.headerTitle}>{props.title}</Text>
-        </View>
-    );
+const Header = props => {
+  return (
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid
+        })
+      }}
+    >
+      <TitleText style={styles.title}>{props.title}</TitleText>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    header: {
-        width: '100%',
-        height: 90,
-        paddingTop: 36,
-        backgroundColor: Colors.primary,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    headerTitle: {
-        color: 'black',
-        fontSize: 18,
-
-    }
+  headerBase: {
+    width: '100%',
+    height: 90,
+    paddingTop: 36,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerIOS: {
+    backgroundColor: 'white',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary
+  },
+  title: {
+    color: Platform.OS === 'ios' ? Colors.primary : 'white'
+  }
 });
 
-export default  Header;
+export default Header;
