469669361aeb1c588046e8855b38694656bcc279
romkravets <romann.kravets@gmail.com>
2020-08-12 11:04:55 +0300

    Adding Header Buttons

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,47 +1,53 @@
-import React from "react";
-import {View, Text, StyleSheet, Button, FlatList, TouchableOpacity, Platform} from 'react-native';
+import React from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
-    const renderGridItem = itemData => {
-        return (
-            <TouchableOpacity style={styles.gridItem} onPress={() => {
-                props.navigation.navigate({routeName: 'CategoryMeals', params: {
-                    categoryId: itemData.item.id
-                }});
-            }}>
-                <View>
-                <Text>{itemData.item.title}</Text>
-                </View>
-            </TouchableOpacity>
-        );
-    }
+  const renderGridItem = itemData => {
     return (
-         <FlatList
-            keyExtractor={item => item.id}
-            data={CATEGORIES}
-            renderItem={renderGridItem}
-            numColumns={2} />
-    )
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
+          props.navigation.navigate({
+            routeName: 'CategoryMeals',
+            params: {
+              categoryId: itemData.item.id
+            }
+          });
+        }}
+      />
+    );
+  };
+
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
+  );
 };
 
-
 CategoriesScreen.navigationOptions = {
-    headerTitle: 'Meal Categories',
+  headerTitle: 'Meal Categories'
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    gridItem: {
-        flex: 1,
-        margin: 15,
-        height: 150,
-    }
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 export default CategoriesScreen;
