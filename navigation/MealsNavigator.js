9ec06c9c4b897c8943e316dc4b2dc119485711e2
romkravets <romann.kravets@gmail.com>
2020-08-12 11:44:39 +0300

    Adding Tabs-based Navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,11 +1,17 @@
+import React from "react";
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
 import { createAppContainer } from 'react-navigation';
 import { Platform } from 'react-native';
 import Colors from '../constants/Colors';
 
+
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import FavoritsScreen from '../screens/FavoritesScreen';
 
 const MealsNavigator = createStackNavigator(
   {
@@ -30,4 +36,27 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator  = createBottomTabNavigator({
+  Meals: {
+    screen: MealsNavigator,
+    navigationOptions: {
+      tabBarIcon: (tabInfo) => {
+        return <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor}/>
+      }
+    }
+  },
+  Favorites: {
+    screen: FavoritsScreen,
+    navigationOptions: {
+      tabBarIcon: (tabInfo) => {
+            return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor}/>
+          }
+    }
+  }
+}, {
+  tabBarOptions: {
+    activeTintColor: Colors.accentColor
+  }
+});
+
+export default createAppContainer(MealsFavTabNavigator);
