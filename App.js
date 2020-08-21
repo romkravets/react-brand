530d110fcb5e67d061848ac1d53c6bfb27b01277
romkravets <romann.kravets@gmail.com>
2020-08-21 20:55:14 +0300

    start folder, redux

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import {createStore, combineReducer} from 'redux';
+import {Provider} from 'react-redux';
+
+import productsReducers from './store/reducers/prodects';
+
+const rootReducer = combineReducer({
+  products: productsReducers
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Hello")
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <StatusBar style="auto" />
-      <Button title="Change Text" onPress={() => setOutputText('World')}/>
+    <Provider store={store}>
+    <View>
+     ...
     </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+
+const ProductsOverviewScreen = props => {
+  return (
+    <FlatList/>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default ProductsOverviewScreen;
